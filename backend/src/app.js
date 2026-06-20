ae21456302eef5afcfe29f243fcd6fc35af23c27
SameerHRV <your_github_username@users.noreply.github.com>
2026-06-20 14:04:52 +0530

    fix: update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import { clerkMiddleware } from "@clerk/express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import fs from "fs";
+import path from "path";
 import config from "./config/config.js";
 
 const app = express();
@@ -28,15 +30,27 @@ app.use(
     limit: limit,
   }),
 );
-app.use(express.static("public"));
 app.use(cookieParser());
 app.use(clerkMiddleware());
+const publicPath = path.join(process.cwd(), "public");
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
   });
 });
+
+if (fs.existsSync(publicPath)) {
+  app.use(express.static(publicPath));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"), (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
+}
 
 import { globalAsnycErrorHandler } from "./utils/globalAsnycErrorHandler.js";
 
